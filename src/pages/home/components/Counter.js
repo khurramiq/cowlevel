ac3306214ba4d25f3ac72646599d5b953bbdb340
khurramiq <khurramkiiman@gmail.com>
2023-05-24 23:30:18 +0500

    new changes

--- src/pages/home/components/Counter.js
+++ src/pages/home/components/Counter.js
@@ -95,10 +95,7 @@ const Counter = ({ startCounter, fullpageApi }) => {
   }, [startCounter]);
 
   return (
-    <div
-      className="absolute flex justify-center items-center top-[100%] w-screen overflow-hidden bg-[#FFFBE8]"
-      style={{ height: 'calc(100vh - 100px)' }}
-    >
+    <div className="absolute flex justify-center items-center top-[100%] w-screen h-screen overflow-hidden bg-[#FFFBE8]">
       <div>
         <div className="flex items-center justify-center">
           <span className="xs:text-[32px] sm:text-[3em] mr-2">$</span>
