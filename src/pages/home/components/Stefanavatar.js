3f1eaa1981bd01ce85f82ded12d6a6a6521e3afe
khurramiq <khurramkiiman@gmail.com>
2023-05-25 01:49:58 +0500

    new cahgnes

--- src/pages/home/components/Stefanavatar.js
+++ src/pages/home/components/Stefanavatar.js
@@ -19,10 +19,7 @@ const Stefanavatar = ({ fullpageApi }) => {
           <FaBars className="text-3xl" />
         </div>
       </div>
-      <div
-        className="top-0 absolute xs:flex xs:items-center w-screen overflow-hidden bg-[#CAD5CC]"
-        style={{ height: '100vh' }}
-      >
+      <div className="top-0 absolute xs:flex xs:items-center h-screen w-screen overflow-hidden bg-[#CAD5CC]">
         <div className="relative top-0 w-full h-full">
           <img
             className="absolute top-0 w-full xs:inline-block sm:hidden"
